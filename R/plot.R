851a49de0568bd43b8a09f42279ab4f39bee45a0
Gustav Delius <gustav.delius@gmail.com>
2024-08-23 09:23:02 +0100

    Add number of prey to plots

--- R/plot.R
+++ R/plot.R
@@ -11,6 +11,7 @@ plot_log_ppmr_fit <- function(ppmr_data, fit) {
     ppmr_data <- validate_ppmr_data(ppmr_data, species = fit$species) |>
         filter(species == !!fit$species,
                w_pred >= fit$min_w_pred)
+    n_prey <- sum(ppmr_data$n_prey)
     ppmr_data$biomass <- ppmr_data$n_prey * ppmr_data$w_prey
     lmin <- max(0, Hmisc::wtd.quantile(ppmr_data$log_ppmr, ppmr_data$biomass, 0.001) - 2)
     lmax <- Hmisc::wtd.quantile(ppmr_data$log_ppmr, ppmr_data$n_prey, 0.999) + 2
@@ -20,14 +21,15 @@ plot_log_ppmr_fit <- function(ppmr_data, fit) {
     fit1 <- transform_fit(fit, 1)
     dist1 <- data.frame(log_ppmr = grid, Density = get_density(grid, fit1))
 
-    plot_title <- paste(fit$species, ": Fit of", fit$distribution, "to")
+    plot_subtitle <- paste("Fit of", fit$distribution, "to")
     if (fit$power == 0) {
-        plot_title <- paste(plot_title, "number density")
+        plot_subtitle <- paste(plot_subtitle, "number density")
     } else if (fit$power == 1) {
-        plot_title <- paste(plot_title, "biomass density")
+        plot_subtitle <- paste(plot_subtitle, "biomass density")
     } else {
-        plot_title <- paste(plot_title, "density with power", fit$power)
+        plot_subtitle <- paste(plot_subtitle, "density with power", fit$power)
     }
+    plot_subtitle <- paste(plot_subtitle, "with", n_prey, "prey.")
 
     ggplot(ppmr_data) +
         geom_density(aes(log_ppmr, weight = n_prey, fill = "Number"),
@@ -38,7 +40,7 @@ plot_log_ppmr_fit <- function(ppmr_data, fit) {
         geom_line(aes(log_ppmr, Density, color = "Biomass"), data = dist1) +
         xlab("Log of predator/prey mass ratio") +
         xlim(lmin, lmax) +
-        ggtitle(plot_title) +
+        ggtitle(fit$species, subtitle = plot_subtitle) +
         scale_fill_manual(name = "Observed density",
                           values = c("Number" = "lightblue",
                                      "Biomass" = "#ffcccb")) +
@@ -54,12 +56,14 @@ plot_log_ppmr_fit <- function(ppmr_data, fit) {
 #' @param power The power to raise the weights to
 #' @export
 plot_ppmr_violins <- function(ppmr_data, species, power = 1) {
-    stomach_binned <- ppmr_data |>
+    ppmr_data <- ppmr_data |>
         validate_ppmr_data(species = species) |>
-        filter(species == !!species) |>
-        # bin data
-        # We need to wiggle the data a bit to avoid
-        # duplicates which `cut_number()` does not like
+        filter(species == !!species)
+    n_prey <- sum(ppmr_data$n_prey)
+    # bin data
+    # We need to wiggle the data a bit to avoid
+    # duplicates which `cut_number()` does not like
+    stomach_binned <- ppmr_data |>
         mutate(bin = cut_number(w_pred * rnorm(length(w_pred), 1, 0.001), 10))
 
     if (power == 1) {
@@ -77,5 +81,6 @@ plot_ppmr_violins <- function(ppmr_data, species, power = 1) {
                     draw_quantiles = 0.5) +
         xlab("Predator weight [g]") +
         ylab("Log of predator/prey mass ratio") +
-        ggtitle(plot_title)
+        ggtitle(plot_title, subtitle = paste("Total number of prey:", n_prey))
+
 }
